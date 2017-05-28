9f85481936647ce0d42c2250f6c67e5c41b2a6b8
Madnarth <corwyn.blade@gmail.com>
2017-05-28 15:05:11 +0200

    v 0.5
    Spin methods

--- One-ArmedBandit/MachineController.cs
+++ One-ArmedBandit/MachineController.cs
@@ -11,6 +11,7 @@ namespace One_ArmedBandit
     public class MachineController
     {
         private int tokensPool;
+        public enum Fruits { f1 = 1, f2 = 2, f3 = 3 };
 
         public int GetTokensPool()
         {
@@ -21,7 +22,7 @@ namespace One_ArmedBandit
             tokensPool += value;
             l1.Text = "Tokens in the pool: " + tokensPool.ToString();
         }
-
+        #region hide
         public string GetActiveName(string activePlayer)
         {
             var selStmt = new SqlCommand("SELECT PlayerName FROM Player WHERE PlayerName = '" + activePlayer + "'", MBDB.conn);
@@ -60,6 +61,7 @@ namespace One_ArmedBandit
 
             return Convert.ToInt32(value);
         }
+        #endregion
         public void SetFruits(PictureBox p1, PictureBox p2, PictureBox p3)
         {
             var random = new Random();
@@ -67,13 +69,50 @@ namespace One_ArmedBandit
             p2.Image = System.Drawing.Image.FromFile($"Fruit{random.Next(1, 4)}.png");
             p3.Image = System.Drawing.Image.FromFile($"Fruit{random.Next(1, 4)}.png");
         }
+        public void SetPicture(PictureBox picture, int value)
+        {
+            picture.Image = System.Drawing.Image.FromFile($"Fruit{value}.png");
+
+        }
         public void SetPlayer(Label l1, Label l2, Label l3, string activePlayer)
         {
-            var selStmt = new SqlCommand("SELECT Cash FROM Player WHERE PlayerName = '" + activePlayer + "'", MBDB.conn);
+            var selStmt = new SqlCommand("SELECT PlayerName, Cash, Tokens FROM Player WHERE PlayerName = '" + activePlayer + "'", MBDB.conn);
             SqlDataReader reader = selStmt.ExecuteReader();
-            l1.Text = "Player: " + GetActiveName(MBDB.activePlayer);
-            l2.Text = "Cash: " + GetActiveCash(MBDB.activePlayer);
-            l3.Text = "Tokens: " + GetActiveCash(MBDB.activePlayer);
+            while (reader.Read())
+            {
+                l1.Text = "Player: " + reader["PlayerName"].ToString();
+                l2.Text = "Cash: " + reader["Cash"].ToString();
+                l3.Text = "Tokens: " + reader["Tokens"].ToString();
+            }
+            reader.Close();
+        }
+        public void Spin(PictureBox p1, PictureBox p2, PictureBox p3, Label l1)
+        {
+            Fruits[] fruit = new Fruits[3];
+            var random = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Array values = Enum.GetValues(typeof(Fruits));
+                fruit[i] = (Fruits)values.GetValue(random.Next(values.Length));
+            }
+            ResolveResults(fruit, l1);
+            p1.Image = System.Drawing.Image.FromFile($"Fruit{(int)fruit[0]}.png");
+            p2.Image = System.Drawing.Image.FromFile($"Fruit{(int)fruit[1]}.png");
+            p3.Image = System.Drawing.Image.FromFile($"Fruit{(int)fruit[2]}.png");
+        }
+        public void ResolveResults(Fruits[] fruit, Label l1)
+        {
+            int value = 0;
+            if ((int)fruit[0] == 3) value = 5;
+
+            if ((int)fruit[0] == 2 & (int)fruit[1] == 2) value = 10;
+            if ((int)fruit[0] == 3 & (int)fruit[1] == 3) value = 10;
+
+            if ((int)fruit[0] == 1 & (int)fruit[1] == 1 & (int)fruit[2] == 1) value = 20;
+            if ((int)fruit[0] == 2 & (int)fruit[1] == 2 & (int)fruit[2] == 2) value = 30;
+            if ((int)fruit[0] == 3 & (int)fruit[1] == 3 & (int)fruit[2] == 3) value = 50;
+            ChangeTokensPool(value, l1);
         }
     }
 }
--- One-ArmedBandit/MachineScreen.cs
+++ One-ArmedBandit/MachineScreen.cs
@@ -18,79 +18,16 @@ namespace One_ArmedBandit
             InitializeComponent();
         }
 
-        // DECLARING TOTAL, BET AND CREDITS
-        public static long credits = 100;
-        public static long total = 0;
-        public static int bet = 10;
-
-        // DECLARING EACH ITEM
-        public static int p1;
-        public static int p2;
-        public static int p3;
-
         private void Form1_Load(object sender, EventArgs e)
         {
             MC.SetFruits(pictureBox1, pictureBox2, pictureBox3);
             MC.SetPlayer(labPlayerName, labCash, labTokens, MBDB.activePlayer);            
         }
 
-        // GENERATES RANDOM NUMBERS
-        public static class IntUtil
-        {
-            private static Random random;
-
-            private static void Init()
-            {
-                if (random == null) random = new Random();
-            }
-            public static int Random(int min, int max)
-            {
-                Init();
-                return random.Next(min, max);
-            }
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            MC.ChangeTokensPool(10, labTokensPool);
-            //if (credits >= bet)
-            //{
-            //    credits = credits - bet;
-            //    label5.Text = "Credits: " + credits.ToString();
-
-            //    for (var i = 0; i < 10; i++)
-            //    {
-            //        p1 = IntUtil.Random(1, 4);
-            //        p2 = IntUtil.Random(1, 4);
-            //        p3 = IntUtil.Random(1, 4);
-            //    }
-
-            //    if (pictureBox1.Image != null) pictureBox1.Image.Dispose();
-            //    pictureBox1.Image = Image.FromFile(p1.ToString() + ".png");
-
-            //    if (pictureBox2.Image != null) pictureBox2.Image.Dispose();
-            //    pictureBox2.Image = Image.FromFile(p2.ToString() + ".png");
-
-            //    if (pictureBox3.Image != null) pictureBox3.Image.Dispose();
-            //    pictureBox3.Image = Image.FromFile(p3.ToString() + ".png");
-
-            //    total = 0;
-
-            //    // GET RESULTS FROM PAYTABLE
-            //    // CHECK IF 1, 2 OR 3 OCCURANCES
-            //    if (p1 == 3) total = total + 5;
-
-            //    if (p1 == 2 & p2 == 2) total = total + 10;
-            //    if (p1 == 3 & p2 == 3) total = total + 10;
-
-            //    if (p1 == 1 & p2 == 1 & p3 == 1) total = total + 20;
-            //    if (p1 == 2 & p2 == 2 & p3 == 2) total = total + 30;
-            //    if (p1 == 3 & p2 == 3 & p3 == 3) total = total + 50;
-
-            //    credits = credits + total;
-            //    label4.Text = "Win: " + total.ToString();
-            //    label5.Text = "Credits: " + credits.ToString();
-            //}
+            MC.Spin(pictureBox1, pictureBox2, pictureBox3, labTokensPool);
+            //MC.ChangeTokensPool(10, labTokensPool);
         }
 
         private void MachineScreen_FormClosing(object sender, FormClosingEventArgs e)
